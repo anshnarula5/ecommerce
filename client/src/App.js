6070f105b426e70a0cedabc0d2754f332f4e1e23
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-24 16:23:02 +0530

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import {Container} from "react-bootstrap"
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
       <Header />
-      <main className = "py-4">
+      <main className="py-4">
         <Container>
-        <h1>Helloxc csfdsf sd fs f</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ client/src/components/Rating.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Rating = ({ value, text, color = "#f8e825" }) => {
+  return (
+    <div className="rating">
+      <span>
+        {[1, 2, 3, 4, 5].map((index) => (
+          <i style = {{color}}
+            className={
+              value >= index
+                ? "fas fa-star"
+                : value >= index - 0.5
+                ? "fas fa-star-half-alt"
+                : "far fa-star"
+            }
+          ></i>
+        ))}
+      </span>
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
