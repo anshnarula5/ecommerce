66d5e9497324abf77dd4dc605c5feae1dad07749
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-25 15:31:04 +0530

    frontend

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
         <Container>
-          <Navbar.Brand href="/">MERN Ecomm</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/">MERN Ecomm</Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="ml-auto">
-              <Link to="/cart"><i class="fas fa-shopping-cart"></i> Cart</Link>
-              <Link to="/auth"><i class="fas fa-sign-in-alt"></i> Sign In</Link>
+              <Nav.Link>
+                <Link to="/cart">
+                  <i class="fas fa-shopping-cart"></i> Cart
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/auth">
+                  <i class="fas fa-sign-in-alt"></i> Sign In
+                </Link>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import Button from "@restart/ui/esm/Button";
+import React from "react";
+import {
+  Card,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from "react-bootstrap";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
+import products from "../products";
 
 const ProductScreen = () => {
-    return (
-        <div>
-          product  
-        </div>
-    )
-}
+  const params = useParams();
+  console.log(params);
+  const product = products.find((product) => product._id === params.id);
+  return (
+    <>
+      <Link className="btn btn-dark my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={` ${product.numReviews} reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price : {product.price}</ListGroupItem>
+            <ListGroupItem>Description : {product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Price : </Col>
+                  <Col>{product.price}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status :</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In stock" : "Not in stock"}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button className="btn btn-dark btn-block" type = "button">Add to cart</Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
