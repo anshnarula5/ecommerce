d96a56901aa4378ce180ddd6ee0cb532034f1cb4
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-25 18:29:20 +0530

    added models

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,32 @@
-const express = require("express")
-const products = require("./data/products")
-const app = express()
+const express = require("express");
+const products = require("./data/products");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((conn) => console.log("mongoose running", conn.connection.host))
+  .catch((err) => console.log("mongoose ERROR", err));
+
 
 app.get("/", (req, res) => {
-    res.send("Api is running")
-})
+  res.send("Api is running");
+});
 
 app.get("/api/products", (req, res) => {
-    res.json(products)
-})
+  res.json(products);
+});
 app.get("/api/products/:id", (req, res) => {
-    const product = products.find(product => product._id === req.params.id)
-    res.json(product)
-})
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000, () => console.log("Running on 5000"))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
