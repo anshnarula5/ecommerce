bb7a11f07101cde19b26293956d1e27418d1fbd7
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-28 20:11:39 +0530

    register routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,30 +1,69 @@
-const asyncHandler = require("express-async-handler")
-const User = require("../models/User")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const loginController = asyncHandler(async (req, res) => {
-    const {email, password} = req.body
-    const user = await User.findOne({email})
-    if (!user) {
-        res.status(404)
-        throw new Error("No user found")
-    }
-    const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) {
-        res.status(401)
-        throw new Error("Invalid credentials")
-    }
-    const id = user._id
-    const token = jwt.sign({id}, "secret", {expiresIn: 360000})
-    res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token
-    })
-})
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(404);
+    throw new Error("No user found");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
+  const id = user._id;
+  const token = jwt.sign({ id }, "secret", { expiresIn: 360000 });
+  res.json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token,
+  });
+});
+const registerController = asyncHandler(async (req, res) => {
+  const { email, password, name } = req.body;
+  let user = await User.findOne({ email });
+  if (user) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const hashedPassword = await bcrypt.hash(password, 12);
+  user = await User.create({ email, password : hashedPassword, name });
+  if (user) {
+    const id = user._id;
+    const token = jwt.sign({ id }, "secret", { expiresIn: 360000 });
 
-module.exports = loginController
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Enter correct credentials");
+  }
+});
+
+const getProfileController = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("No user found");
+  }
+});
+
+module.exports = { loginController, getProfileController, registerController };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express")
-const loginController = require("../controllers/user")
+const {loginController, getProfileController, registerController} = require("../controllers/user")
+const auth = require("../middleware/authMiddleware")
 const router = express.Router()
 
 router.post("/login", loginController)
+router.get("/profile", auth, getProfileController)
+router.post("/", registerController)
 
 module.exports = router
