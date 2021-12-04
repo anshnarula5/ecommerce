0f934cb06d6c3b032d9a6c682634967eefd3cfa5
Ansh Narula <2019210@iiitdmj.ac.in>
2021-12-05 00:47:02 +0530

    deploy changes

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.get("/api/config/paypal", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")))
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html")))
 }
 
 app.use(notFound)
