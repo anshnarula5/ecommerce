2b56828cc508b8958191bf57c4fd519957bfd49f
Ansh Narula <2019210@iiitdmj.ac.in>
2021-12-05 00:07:30 +0530

    heroku change

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,12 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")))
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
-}
+app.get("/", (req, res) => res.send("hello"))
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")))
+//   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+// }
 
 app.use(notFound)
 
@@ -43,3 +45,5 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
+
+// "heroku-postbuild" : "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
